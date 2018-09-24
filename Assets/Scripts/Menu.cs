9c0ffa92816150e15f7e06a3469d97a7217b12fe
Aaron <abrunette@users.noreply.github.com>
2018-09-24 00:39:36 -0700

    Added main menu, quit, return, and new background.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.black;
+    }
+}
